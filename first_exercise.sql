40cb35902da4ca9c98cae7605675b7ab42d1e61a
Liam Jackson <ljackson3613@yahoo.com>
2020-12-23 15:28:10 -0600

    Practice 2 Done

--- first_exercise.sql
+++ first_exercise.sql
@@ -1,4 +1,4 @@
-#Practice 
+#Practice 1
 
 #1)
 #Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
@@ -88,3 +88,83 @@ SELECT DISTINCT(CITY)
 FROM STATION
 WHERE (ID % 2) = 0
 ORDER BY CITY DESC;
+
+#Practice 2
+
+#1)
+#Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. 
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
+
+#2)
+#Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+The STATION table is described as follows:
+
+(SELECT CITY,
+        CHAR_LENGTH(CITY) AS len_city
+FROM STATION
+WHERE CHAR_LENGTH(CITY)=(SELECT CHAR_LENGTH(CITY)
+FROM STATION ORDER BY CHAR_LENGTH(CITY) ASC LIMIT  1)
+ORDER BY CITY ASC LIMIT 1)
+UNION ALL
+(SELECT CITY,
+        CHAR_LENGTH(CITY) AS len_city
+FROM STATION
+WHERE CHAR_LENGTH(CITY)=(SELECT CHAR_LENGTH(CITY)
+FROM STATION ORDER BY CHAR_LENGTH(CITY) DESC LIMIT  1)
+ORDER BY CITY DESC LIMIT 1)
+ORDER BY CHAR_LENGTH(CITY);
+#3)
+#Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE lower(SUBSTR(CITY,1,1)) IN ("a", "e", "i", "o", "u");
+
+#4)
+#Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE CITY LIKE "%a"
+OR CITY LIKE "%e"
+OR CITY LIKE "%i"
+OR CITY LIKE "%o"
+OR CITY LIKE "%u";
+
+#5)
+#Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY RLIKE '^[aeiouAEIOU].*[aeiouAEIOU]$';
+
+#6)
+#Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE NOT CITY LIKE "a%"
+AND NOT CITY LIKE "e%"
+AND NOT CITY LIKE "i%"
+AND NOT CITY LIKE "o%"
+AND NOT CITY LIKE "u%";
+
+#7)
+#Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE NOT CITY LIKE "%a"
+AND NOT CITY LIKE "%e"
+AND NOT CITY LIKE "%i"
+AND NOT CITY LIKE "%o"
+AND NOT CITY LIKE "%u";
+
+#8)
+#Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT CITY RLIKE '^[aeiouAEIOU].*[aeiouAEIOU]$';
